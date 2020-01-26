6657a6940ded8d309d5b1a6836ce08d2308cd040
Aarish Adeel <aarish8@gmail.com>
2020-01-26 10:17:50 -0500

    Update server port

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from templates import app
+import os
 
 app.config.from_object('configurations.DevelopmentConfig')
 
-port = 5000
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
     app.run(port=port)
